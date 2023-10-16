639c3906b433b4be2f79e9e8714d4896d0a9a8f5
SiddhantBodile <siddhantbodile@gmail.com>
2023-10-16 06:56:42 +0000

    pm2

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Change to your application directory
-cd /var/www/html
+cd /var/www/html/
 
 # Install Node.js dependencies
 /usr/bin/npm install
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,7 +4,7 @@
 cd /var/www/html/
 
 # Install pm2 globally
-npm install pm2
+npm install pm2 -g
 
 # Start your Node.js application using pm2
 ./node_modules/.bin/pm2 start app.js --name nodejs-app -f
